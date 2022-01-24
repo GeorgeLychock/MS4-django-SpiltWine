c8f0cc6e9e07f0968078bd48def6414bfa9d9203
George <glychock@gmail.com>
2022-01-23 22:17:49 -0500

    data: Finished models; migrated; upload data json and image files; loaded data; tested in Admin

--- spilt_wine/products/admin.py
+++ spilt_wine/products/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Product, Appellation, Region, WineType, Brand, CountryState, Varietal, Style, Body
 
 # Register your models here.
+admin.site.register(Appellation)
+admin.site.register(Region)
+admin.site.register(WineType)
+admin.site.register(Brand)
+admin.site.register(CountryState)
+admin.site.register(Varietal)
+admin.site.register(Style)
+admin.site.register(Body)
+admin.site.register(Product)
--- spilt_wine/products/models.py
+++ spilt_wine/products/models.py
@@ -65,7 +65,29 @@ class CountryState(models.Model):
 class Varietal(models.Model):
     name = models.CharField(max_length=30)
     friendly_name = models.CharField(max_length=50, null=True, blank=True)
-    wine_type = models.ForeignKey('WineType', null=True, blank=True)
+    wine_type = models.ForeignKey('WineType', null=True, blank=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Style(models.Model):
+    name = models.CharField(max_length=50)
+    friendly_name = models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Body(models.Model):
+    name = models.CharField(max_length=50)
+    friendly_name = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -78,20 +100,21 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     sku = models.CharField(max_length=254, null=True, blank=True)
     img_file_name = models.ImageField(null=True, blank=True)
+    img_url = models.URLField(max_length=1024, null=True, blank=True)
     vintage = models.IntegerField(null=True, blank=True)
-    size = models.DecimalField(max_digits=3, decimal_places=2)
+    size = models.DecimalField(max_digits=6, decimal_places=2)
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    brand = models.ForeignKey('Brand', null=True, blank=True)
-    country_state = models.ForeignKey('CountryState')
-    region = models.ForeignKey('Region', null=True, blank=True)
-    appellation = models.ForeignKey('Appellation', null=True, blank=True)
-    wine_type = models.ForeignKey('WineType')
-    varietal = models.ForeignKey('Varietal')
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    brand = models.ForeignKey('Brand', null=True, blank=True, on_delete=models.SET_NULL)
+    country_state = models.ForeignKey('CountryState', null=True, on_delete=models.SET_NULL)
+    region = models.ForeignKey('Region', null=True, blank=True, on_delete=models.SET_NULL)
+    appellation = models.ForeignKey('Appellation', null=True, blank=True, on_delete=models.SET_NULL)
+    wine_type = models.ForeignKey('WineType', null=True, on_delete=models.SET_NULL)
+    varietal = models.ForeignKey('Varietal', null=True, on_delete=models.SET_NULL)
     style = models.CharField(max_length=25, null=True, blank=True)
-    abv = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
+    abv = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
     taste = models.TextField(max_length=100, null=True, blank=True)
-    body = models.CharField(null=True, blank=True)
+    body = models.CharField(max_length=100,null=True, blank=True)
 
     def __str__(self):
         return self.name
